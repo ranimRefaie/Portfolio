5c06665c9dd2c5f7bc505074865154e842f25943
ranimRefaie <ranemalre@gmail.com>
2024-04-15 13:31:37 +0300

    first commit

--- src/Components/ButtonDownload/ButtonDownload.jsx
+++ src/Components/ButtonDownload/ButtonDownload.jsx
@@ -2,34 +2,17 @@ import "./ButtonDownload.css";
 import { motion } from "framer-motion";
 import { FaCloudArrowDown } from "react-icons/fa6";
 
-const PDF_File_URL = "http://localhost:5173/portfolio/ranim_alrefaie.pdf";
-
 const ButtonDownload = ({ text }) => {
-  const downloadFile = (url) => {
-    fetch(url)
-      .then((response) => response.blob())
-      .then((blob) => {
-        const blobURL = window.URL.createObjectURL(new Blob([blob]));
-        const fileName = url.split("/").pop();
-        const aTag = document.createElement("a");
-        aTag.href = blobURL;
-        aTag.setAttribute("download", fileName);
-        document.body.appendChild(aTag);
-        aTag.click();
-        aTag.remove();
-      });
-  };
   return (
-    <motion.button
+    <motion.a
       className="btn-cv"
       whileInView={{ scale: 1.1 }}
-      onClick={() => {
-        downloadFile(PDF_File_URL);
-      }}
+      href="https://ranimrefaie.github.io/portfolio/ranim_alrefaie.pdf"
+      download="ranim.pdf"
     >
       <FaCloudArrowDown className="icon" />
       <span>{text}</span>
-    </motion.button>
+    </motion.a>
   );
 };
 
